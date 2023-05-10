005c6d212382ffedc6483a1d8b807a401330878b
Darkhan Kubigenov <darkhanu@gmail.com>
2023-05-10 20:58:18 +0100

    Add version flag

--- cat.go
+++ cat.go
@@ -1,7 +1,30 @@
 package main
 
+import flag "github.com/spf13/pflag"
 import "fmt"
+import "strings"
+
 
 func main() {
-    fmt.Println("Hello, World!")
+
+		verFlag := flag.BoolP("version", "v", false, "output version information and exit")
+
+		flag.CommandLine.SortFlags = false
+		flag.Parse()
+
+		if *verFlag {
+			printVersionInformation()
+		}
+
+}
+
+
+const version string =
+`
+go-cat 0.1
+Hello world
+`
+
+func printVersionInformation() {
+	fmt.Printf(strings.TrimLeft(version, "\n"))
 }
